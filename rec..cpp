bad637ee096ba307a8063f1e9b8a0ee4b5abb70d
hamna92 <hamnakamran92@gmail.com>
2022-12-29 20:39:07 +0500

    week 02

---
+++ rec..cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+main() {
+
+cout<< "********************************************************************************************************************************" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                        UNIVERSITY OF ENGINEERING                                                             *" << endl;                                                                                                                              
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                           AND TECHNOLOGY                                                                     *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "*                                                                                                                              *" << endl;
+cout<< "********************************************************************************************************************************" << endl;
+}
